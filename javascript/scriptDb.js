febbcfaf412db7ceb477658b6b8f12e7f625ac34
Werner Wolff <werner.wolff1109@gmail.com>
2025-09-02 12:31:00 +0200

    fix: we have access to the data of our SQlite query

--- javascript/scriptDb.js
+++ javascript/scriptDb.js
@@ -1,4 +1,4 @@
-
+const testDb = document.getElementById("test_db")
 
 initSqlJs({
     locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.2/${file}`
@@ -8,7 +8,14 @@ initSqlJs({
       .then(res => res.arrayBuffer())
       .then(buf => {
         const db = new SQL.Database(new Uint8Array(buf));
-        const res = db.exec("SELECT * FROM animal;");
+        const res = db.exec("SELECT * FROM animal WHERE type='Chat';");
         console.log(res[0]);
+        console.log(Object.values(res[0].values)) // on peut aussi faire .values[i] pour récupérer les élements que l'on souhaite
+
+        for(i in res.length){
+          testDb.innerHTML += `<p>res[i].name</p>`
+        }
       });
-  });   
+  });
+
+  // initSqlJs(testDb)
