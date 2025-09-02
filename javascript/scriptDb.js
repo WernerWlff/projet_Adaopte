61381f5a2a080ca390af9cda7f0e237305a8e521
Elouan <elouangauriaud@gmail.com>
2025-09-02 11:08:10 +0200

    update: Adding new HTML page, linked script in both HTML and starting sql.js in scriptDb.js

---
+++ javascript/scriptDb.js
@@ -0,0 +1,14 @@
+import initSqlJs from "sql.js";
+
+initSqlJs({
+    locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.2/${file}`
+  }).then(SQL => {
+
+    fetch("animal.db")
+      .then(res => res.arrayBuffer())
+      .then(buf => {
+        const db = new SQL.Database(new Uint8Array(buf));
+        const res = db.exec("SELECT * FROM animal;");
+        console.log(res[0]);
+      });
+  });
