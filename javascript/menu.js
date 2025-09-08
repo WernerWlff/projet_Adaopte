19c6ff01ba23bf7323f0deeacbc07ced8ff0dd7a
Elouan <elouangauriaud@gmail.com>
2025-09-08 15:17:48 +0200

    wip: responsive design on adoption_page.html and adding a menu.js to manage menu in mobile

---
+++ javascript/menu.js
@@ -0,0 +1,19 @@
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+        
+        mobileMenuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+
+        const mobileLinks = mobileMenu.querySelectorAll('a');
+        mobileLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                mobileMenu.classList.add('hidden');
+            });
+        });
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth >= 1024) {
+                mobileMenu.classList.add('hidden');
+            }
+        });
