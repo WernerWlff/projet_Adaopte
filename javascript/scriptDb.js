7510c62cb0c5329acf21ed98792e814a28ba04c0
Yanis <yanis.blanchard@gmail.com>
2025-09-10 12:34:27 +0200

    feat : scriptDb.js refactored

--- javascript/scriptDb.js
+++ javascript/scriptDb.js
@@ -1,11 +1,11 @@
-const testDb = document.getElementById("test_db");
+const cardsDb = document.getElementById("cardsDb");
 const researchAnimalSelection = document.getElementById('researchAnimalSelection');
 const researchInput = document.getElementById('researchInput');
 const researchButton = document.getElementById("researchButton");
 const pagination = document.getElementById("pagination");
-let currentPage = 1; // on démarre à la page 1
-const cardPerPage = 8; // nombre d'élements max à afficher par page
-let currentData = []; // vide pour l'instant mais va récupérer la db
+let currentPage = 1; 
+const cardPerPage = 8; 
+let currentData = [];
 
 researchButton.addEventListener("click", () => {
   let selectValue = researchAnimalSelection.value;
@@ -16,10 +16,10 @@ document.getElementById("resetFilters").addEventListener("click", (e) => {
   location.reload();    
 });
 
-  testDb.className = "flex flex-wrap mx-3 w-full"
+  cardsDb.className = "flex flex-wrap mx-3 w-full"
 
-  testDb.innerHTML = '';
-  pagination.innerHTML= "";
+  cardsDb.innerHTML = '';
+  pagination.innerHTML= '';
   document.getElementById("resultCount").textContent = '';
 
   inputValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1).toLowerCase();
@@ -31,27 +31,33 @@ document.getElementById("resetFilters").addEventListener("click", (e) => {
       .then(res => res.arrayBuffer())
       .then(buf => {
         const db = new SQL.Database(new Uint8Array(buf));
-        testDb.innerHTML = '';
+        cardsDb.innerHTML = '';
 
-        // type et city demandés
         if(selectValue !== "Tous" && inputValue !== ''){
           res = db.exec(`SELECT * FROM animal WHERE type='${selectValue}' AND city='${inputValue}';`);
           if(!res.length || !res[0] || !res[0].values.length){
             document.getElementById('resultCount').textContent = "Aucun animal trouvé"
           }
+          else if(res[0].values.length === 1){
+            document.getElementById('resultCount').textContent = res[0].values.length + " animal trouvé"
+            creationCards(res[0].values);
+          }
           else{
           document.getElementById("resultCount").textContent =
           res[0].values.length + " animaux trouvés";
           creationCards(res[0].values);
           }
         }
-
-        //type demandé et pas city
+        
         else if(selectValue !=='Tous' && inputValue === ''){
           res = db.exec(`SELECT * FROM animal WHERE type='${selectValue}';`);
           if(!res.length || !res[0] || !res[0].values.length){
             document.getElementById('resultCount').textContent = "Aucun animal trouvé"
           }
+          else if(res[0].values.length === 1){
+            document.getElementById('resultCount').textContent = res[0].values.length + " animal trouvé"
+            creationCards(res[0].values);
+          }
           else{
           document.getElementById("resultCount").textContent =
           res[0].values.length + " animaux trouvés";
@@ -59,12 +65,15 @@ document.getElementById("resetFilters").addEventListener("click", (e) => {
           }
         }
 
-        //type non demandé et city demandée
         else if(selectValue ==='Tous' && inputValue !== ''){
           res = db.exec(`SELECT * FROM animal WHERE city='${inputValue}';`);
           if(!res.length || !res[0] || !res[0].values.length){
             document.getElementById('resultCount').textContent = "Aucun animal trouvé"
           }
+          else if(res[0].values.length === 1){
+            document.getElementById('resultCount').textContent = res[0].values.length + " animal trouvé"
+            creationCards(res[0].values);
+          }
           else{
           document.getElementById("resultCount").textContent =
           res[0].values.length + " animaux trouvés";
@@ -72,11 +81,14 @@ document.getElementById("resetFilters").addEventListener("click", (e) => {
           }
         }
 
-        // type et city non demandé 
         else{
           res = db.exec("SELECT * FROM animal;");
           if(!res.length || !res[0] || !res[0].values.length){
             document.getElementById('resultCount').textContent = "Aucun animal trouvé"
+          }
+          else if(res[0].values.length === 1){
+            document.getElementById('resultCount').textContent = res[0].values.length + " animal trouvé"
+            creationCards(res[0].values);
           }
           else{
           document.getElementById("resultCount").textContent =
@@ -96,64 +108,54 @@ document.getElementById("resetFilters").addEventListener("click", (e) => {
 
 
 function creationCards(database){
-  testDb.innerHTML = "" // pour vider l'affichage
-  currentData = database // pour récupérer le tableau de données
+  cardsDb.innerHTML = ""; 
+  currentData = database;
 
-  const minPage = (currentPage - 1) * cardPerPage;
-  const maxPage = Math.min(minPage + cardPerPage, currentData.length)
+  const minCards = (currentPage - 1) * cardPerPage;
+  const maxCards = Math.min(minCards + cardPerPage, currentData.length);
   
-        for(let i = minPage ; i < maxPage; i++) {
+        for(let i = minCards ; i < maxCards; i++) {
         const value = database[i];
 
-        // Création du container
         const container = document.createElement("div");
         container.className = "w-full sm:w-1/2 md:w-1/4 px-3 mb-6";
         container.setAttribute("id", `${value[0]}`);
         
-        // Création de la carte
         const card = document.createElement("div");
-        card.className = "overflow-hidden shadow-lg bg-white flex flex-col h-full"
+        card.className = "overflow-hidden shadow-lg bg-white flex flex-col h-full";
 
-        // Image
         const img = document.createElement("img");
         img.className = "w-full h-48 object-cover";
         img.src = value[8];
         img.alt = `${value[1]}_${value[4]}`;
 
-        // Zone d'infos
         const textContainer = document.createElement("div");
         textContainer.className = "p-4 flex flex-col flex-grow justify-between";
 
         const textArea = document.createElement("div");
 
-        // Type
         const cardType = document.createElement("p");
         cardType.className = "text-sm text-gray-700";
         cardType.textContent = value[4];
 
-        // Nom
         const cardName = document.createElement("p");
         cardName.className = "text-lg text-(--accent-color) font-bold";
         cardName.textContent = value[1];
 
-        // Âge et race
         const cardAgeAndBreed = document.createElement("p");
         cardAgeAndBreed.className = "text-gray-700";
         cardAgeAndBreed.textContent = `${value[2]} · ${value[3]}`;
 
-        // Localisation
         const cardLocalisation = document.createElement("p");
         cardLocalisation.className = 'text-gray-500 text-sm mt-1';
         cardLocalisation.textContent = `${value[5]}, ${value[6]}`;
 
-        // Description
         const cardDescription = document.createElement("p");
         cardDescription.className = 'text-gray-700 text-sm mt-2';
         cardDescription.textContent = value[7];
           
-        // Bouton
         const meetUpButton = document.createElement("button");
-        meetUpButton.className = 'bg-gray-900 w-1/4 text-white text-xs font-medium py-2 p-px mt-2 rounded-full hover:bg-gray-800 transition cursor-pointer';
+        meetUpButton.className = 'bg-gray-900 w-1/3 sm:w-full md:w-full lg:w-1/3 text-white text-xs font-bold py-2 mt-2 rounded-full hover:bg-gray-800 transition cursor-pointer';
         meetUpButton.textContent = "Rencontrer";
         
         meetUpButton.addEventListener('click', () => {
@@ -176,7 +178,7 @@ function creationCards(database){
 
         container.appendChild(card);
 
-        testDb.appendChild(container);
+        cardsDb.appendChild(container);
 
         createPagination();
       }
@@ -184,9 +186,8 @@ function creationCards(database){
 
 function createPagination() {
   pagination.innerHTML=''
-  const totalPages = Math.ceil(currentData.length / cardPerPage); // rends l'entier le plus proche
-
-  // Bouton Précédent
+  const totalPages = Math.ceil(currentData.length / cardPerPage);
+  
   if (currentPage > 1) {
     const previousButton = document.createElement("button");
     previousButton.textContent = "« Précédent";
@@ -200,7 +201,6 @@ function createPagination() {
     pagination.appendChild(previousButton);
   }
 
-  // Boucles sur toutes les pages
   for (let i = 1; i <= totalPages; i++) {
     const pageButton = document.createElement("button");
     pageButton.textContent = i;
@@ -219,7 +219,6 @@ function createPagination() {
     pagination.appendChild(pageButton);
   }
 
-  // Bouton Suivant
   if (currentPage < totalPages) {
     const nextButton = document.createElement("button");
     nextButton.textContent = "Suivant »";
